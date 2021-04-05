6ec5841b7de31a66c183567c9ef42989dcbc9339
alexeykainov <pochta_alexeya@mail.ru>
2021-04-05 21:30:40 +0300

    Proba

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = '@tlq219+9tq6d*9o=9-t4bq*i6ng3)nxdqx(ic+iiyxcih*vix'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ask',
+    'qa',
 ]
 
 MIDDLEWARE_CLASSES = (                                                          
