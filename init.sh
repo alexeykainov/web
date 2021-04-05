b6cc221882d2bdc4a01c94be453d62bce173e508
alexeykainov <pochta_alexeya@mail.ru>
2021-04-05 22:15:28 +0300

    Third day

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo gunicorn -c hello.py hello:wsgi_application
+ sudo gunicorn -c /home/box/web/etc/nginx.conf hello:wsgi_application
+ sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi:application
